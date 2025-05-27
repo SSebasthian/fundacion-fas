5532a5cd0e0eb9c302519c98594f7fd7f2689a0e
SSebasthian <ssebasthian.perez@gmail.com>
2025-05-27 08:35:37 -0500

    Se Enruta Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './compartido/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, MenuComponent]
 })
+
 export class AppComponent {
   title = 'fundacion-fas';
 }
