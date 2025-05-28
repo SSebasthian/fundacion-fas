7537417f226c46136c6745fbf6f0de71ec62af41
SSebasthian <ssebasthian.perez@gmail.com>
2025-05-28 10:19:38 -0500

    Se Enruta pagina (INICIO)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pagina/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',
+        component:InicioComponent
+    },
+    {path: 'inicio',
+        component:InicioComponent
+    },
+];
