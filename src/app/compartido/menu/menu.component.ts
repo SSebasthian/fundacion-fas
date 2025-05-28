71a1c6263f90f7cea4b535a276d5968780f7efef
SSebasthian <ssebasthian.perez@gmail.com>
2025-05-28 08:36:55 -0500

    Se Diseña Menu + Responsive

--- src/app/compartido/menu/menu.component.ts
+++ src/app/compartido/menu/menu.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,  ViewChild, ElementRef } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterModule
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  menuAbierto: boolean = false;
+  @ViewChild('menuCheckbox') menuCheckbox!: ElementRef<HTMLInputElement>;
 
+   menuHamburguesa(){
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  cerrarMenu(): void{
+    this.menuAbierto = false;
+
+    // Si el checkbox está marcado, lo desmarcamos
+    if (this.menuCheckbox?.nativeElement?.checked) {
+      this.menuCheckbox.nativeElement.checked = false;
+    }
+  }
 }
