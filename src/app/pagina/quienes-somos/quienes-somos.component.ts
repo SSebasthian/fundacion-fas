f4d0c96a3cffa5540a305021f8d5936bbe9b1e4e
SSebasthian <ssebasthian.perez@gmail.com>
2025-05-30 10:18:57 -0500

    Se Diseña Pagina QUIENES SOMOS

--- src/app/pagina/quienes-somos/quienes-somos.component.ts
+++ src/app/pagina/quienes-somos/quienes-somos.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-quienes-somos',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    CommonModule
+  ],
   templateUrl: './quienes-somos.component.html',
   styleUrl: './quienes-somos.component.css'
 })
 export class QuienesSomosComponent {
 
+  valorSeleccionado: string | null = null;
+
+  seleccionarValor(valor: string) {
+    this.valorSeleccionado = valor;
+  }
 }
