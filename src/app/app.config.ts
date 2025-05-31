2322b343cf34498a0422aa40d739e4463e3410d3
SSebasthian <ssebasthian.perez@gmail.com>
2025-05-31 16:02:56 -0500

    Se hace CRUD con ( FIREBASE / STORAGE / AUTHENTICATION )

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,38 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp, getApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http'
+import { provideStorage, getStorage } from '@angular/fire/storage'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"fundacion-fas","appId":"1:46226900332:web:7cfd102a218a3cf39cdf49","storageBucket":"fundacion-fas.firebasestorage.app","apiKey":"AIzaSyBTSD2PWJ48pB5CJfOiMP1xrsSclgl0APs","authDomain":"fundacion-fas.firebaseapp.com","messagingSenderId":"46226900332"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFirebaseApp(() => initializeApp({"projectId":"fundacion-fas","appId":"1:46226900332:web:7cfd102a218a3cf39cdf49","storageBucket":"fundacion-fas.firebasestorage.app","apiKey":"AIzaSyBTSD2PWJ48pB5CJfOiMP1xrsSclgl0APs","authDomain":"fundacion-fas.firebaseapp.com","messagingSenderId":"46226900332"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true}),
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideFirebaseApp(() =>
+       initializeApp({
+        "projectId":"fundacion-fas",
+        "appId":"1:46226900332:web:7cfd102a218a3cf39cdf49",
+        "storageBucket":"gs://fundacion-fas.firebasestorage.app",
+        "apiKey":"AIzaSyBTSD2PWJ48pB5CJfOiMP1xrsSclgl0APs",
+        "authDomain":"fundacion-fas.firebaseapp.com",
+        "messagingSenderId":"46226900332"
+      })
+    ), 
+    //configura componentes de autenticacion
+    provideAuth(() => getAuth()), 
+    //configura componentes de firestore
+    provideFirestore(() => getFirestore()), 
+    //configura componentes de Firestore 
+    provideFirestore(() => getFirestore()),
+  ],
 };
