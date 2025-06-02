6c38b02d9837bb6714aa00045d7e1a5d41de8ab3
SSebasthian <ssebasthian.perez@gmail.com>
2025-06-02 15:51:02 -0500

    Se Diseña Pagina Programas

--- src/app/pagina/programas/programas.component.ts
+++ src/app/pagina/programas/programas.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-programas',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './programas.component.html',
   styleUrl: './programas.component.css'
 })
