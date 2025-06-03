494a00e8ddd3d246d6dc95a89d4577c0f258dcbe
SSebasthian <ssebasthian.perez@gmail.com>
2025-06-03 11:52:14 -0500

    Metodo para enviar Form a FIREBASE DATABASE

--- src/app/arquitectura/servicio/contacto.service.ts
+++ src/app/arquitectura/servicio/contacto.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import Contacto from '../../arquitectura/interface/contacto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactoService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) {}
+
+  guardarMensaje(contacto: Contacto) {
+  console.log('Guardando en contactosVinculacion:', contacto); // Verifica que aparece en consola
+  const ref = collection(this.firestore, 'contactosVinculacion');
+  return addDoc(ref, contacto);
+}
 }
--- src/app/pagina/vinculacion/vinculacion.component.ts
+++ src/app/pagina/vinculacion/vinculacion.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { ContactoService } from '../../arquitectura/servicio/contacto.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+
 
 @Component({
   selector: 'app-vinculacion',
@@ -9,11 +15,39 @@ import { MatInputModule } from '@angular/material/input';
   imports: [
     MatIconModule,
     MatFormFieldModule,
-    
+    ReactiveFormsModule
   ],
   templateUrl: './vinculacion.component.html',
   styleUrl: './vinculacion.component.css'
 })
 export class VinculacionComponent {
+  formularioContacto: FormGroup;
 
+   constructor(private fb: FormBuilder, private contactoService: ContactoService) {
+    this.formularioContacto = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      telefono: ['', Validators.required],
+      mensaje: ['', Validators.required]
+    });
+  }
+
+  enviar() {
+  if (this.formularioContacto.valid) {
+    const datos = {
+      ...this.formularioContacto.value,
+      fecha: new Date()
+    };
+
+    this.contactoService.guardarMensaje(datos)
+      .then(() => {
+        alert('Mensaje enviado correctamente');
+        this.formularioContacto.reset();
+      })
+      .catch((error) => {
+        console.error('Error al enviar mensaje', error);
+      });
+  }
+}
 }
