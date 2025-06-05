98acb2b236a658192cabf6e682d23a1ee29bb51a
SSebasthian <ssebasthian.perez@gmail.com>
2025-06-05 08:28:55 -0500

    Se Enruta FOOTER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './compartido/menu/menu.component';
+import { FooterComponent } from "./compartido/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, MenuComponent]
+  imports: [CommonModule, RouterOutlet, MenuComponent, FooterComponent]
 })
 
 export class AppComponent {
