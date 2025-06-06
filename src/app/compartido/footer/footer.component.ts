97646c38d9cdf22511b496e1a0c2d1d1b22708b7
SSebasthian <ssebasthian.perez@gmail.com>
2025-06-06 10:21:46 -0500

    Se Diseña FOOTER

--- src/app/compartido/footer/footer.component.ts
+++ src/app/compartido/footer/footer.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    RouterLink,
+    MatIconModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
